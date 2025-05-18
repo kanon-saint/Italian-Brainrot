5714d2054a25193daabda00d28eb36b34a380ddc
Kurt Amodia <kaamodia@up.edu.ph>
2025-05-18 12:32:34 +0800

    updated game assets

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class AttackTrigger : MonoBehaviour
 {
-    [SerializeField] public int attackDamage = 3;
+    [SerializeField] public int attackDamage;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Mob"))
+        if (other.CompareTag("Mob") || other.CompareTag("Boss"))
         {
             PlayerAttibutes enemyAttributes = other.GetComponent<PlayerAttibutes>();
             if (enemyAttributes != null)
